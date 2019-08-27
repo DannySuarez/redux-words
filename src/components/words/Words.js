f2972c0303a258162debad286daa58fd1cfcfaf0
DannySuarez <suarezd10@gmail.com>
2019-08-27 15:15:53 -0700

    Rendering data from json file

--- src/components/words/Words.js
+++ src/components/words/Words.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Words({ array = ['a', 'b'] }) {
-
-  const word = array.map(word => {
-    return <li key={Math.random()}> {word} </li>;
+function Words({ dictionary }) {
+  const word = dictionary.map(word => {
+    return <li key={word}> {word} </li>;
   });
-
+  
   return (
     <ul>
       {word}
     </ul>
   );
 }
+
+Words.propTypes = {
+  dictionary: PropTypes.array.isRequired
+};
 
 export default Words;

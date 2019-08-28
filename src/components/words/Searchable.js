576cadb99a580183bb0e49893d18d3474ed1057d
DannySuarez <suarezd10@gmail.com>
2019-08-28 11:52:21 -0700

    fixed a typo

--- src/components/words/Searchable.js
+++ src/components/words/Searchable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-Types';
+import PropTypes from 'prop-types';
 import SearchWords from '../../containers/SearchWords';
 import TopWords from '../../containers/TopWords';
 

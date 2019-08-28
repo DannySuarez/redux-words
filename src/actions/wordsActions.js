297b18938ab962683c642e3144623a37dd0557f9
DannySuarez <suarezd10@gmail.com>
2019-08-28 09:07:39 -0700

    search functionality

---
+++ src/actions/wordsActions.js
@@ -0,0 +1,5 @@
+export const UPDATE_SEARCH = 'UPDATE_SEARCH';
+export const updateSearch = (filter) => ({
+  type: UPDATE_SEARCH,
+  payload: filter
+});
--- src/reducers/wordsReducer.js
+++ src/reducers/wordsReducer.js
@@ -1,12 +1,15 @@
 import words from '../fixtures/words';
+import { UPDATE_SEARCH } from '../actions/wordsActions';
 
 const initialState = {
-  words: [...words],
+  words,
   filter: ''
 };
 
 export default function reducer(state = initialState, action){
   switch(action.type) {
+    case UPDATE_SEARCH:
+      return { ...state, filter: action.payload };
 
     default:
       return state;
